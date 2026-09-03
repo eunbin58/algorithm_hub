86fe9dd510af29f664cc149d614b8796071ba4ed
eunbin58 <98297421+eunbin58@users.noreply.github.com>
2026-09-03 09:30:30 +0900

    [Passed] 2진수로 변환하기 (49ms, 16MB)

---
+++ codetree/trail2/2진수로 변환하기/convert-to-binary.py
@@ -0,0 +1,13 @@
+n = int(input())
+result=[]
+if(n==0):
+    result.append(0)
+while n>0:
+    result.append(n%2)
+    # print(n%2)
+    n=n//2
+    # print(n)
+
+print("".join(map(str,result[::-1])))
+
+# Please write your code here.
